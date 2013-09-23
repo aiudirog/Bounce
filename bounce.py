f1df46547b2050341f3e3a31a6d67f98f585dcf4
aiudirog <aiudirog@github.com>
2013-09-23 18:21:36 -0400

    Update 1

--- bounce.py
+++ bounce.py
@@ -3,7 +3,8 @@ from pygame.locals import *
 pygame.init()
 
 #image_list = {}
-
+ballrect = []
+dimmension = []
 #def load(file_name):
     #if file_name in image_list:
         #return image_list[file_name]
@@ -15,48 +16,62 @@ screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 WIDTH, HEIGHT = screen.get_size() 
 
 speed = [2,2]
-black = 0, 0, 0
+black = 255, 0, 0
 
 #screen = pygame.display.set_mode((size), pygame.FULLSCREEN)
 
 #ball = [pygame.image.load("ultraball.png"), ]
 
 
-file_name = {pygame.image.load("ultraball.png")}
+file_names = [pygame.image.load("ultraball.png"), pygame.image.load("photo.jpg")]
+x = 2
+a = 0
+for ball in file_names:
+    ballrect.append(ball.get_rect())
+    dimmension.append(ballrect[a].move(speed))
+    a += 1
 
 #for file in file_name:
     #load(file)
 #ballrect = ball.get_rect()
-
-while 1:
-    for ball in file_name:
-        ballrect = ball.get_rect()
+i = 0
+n = 0
+while True:
+    if i == x:
+        i = 0
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                pygame.quit()
         
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            elif event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    pygame.quit()
-        
-        ballrect = ballrect.move(speed)
-        if ballrect.left < 0 or ballrect.right > WIDTH:
-            speed[0] = -speed[0]
-            if speed[1] < 0:
-                speed[1] = random.randint(1,20)
-                speed[1] = -speed[1]
-            else:
-                speed[1] = random.randint(1,20)
-
-        if ballrect.top < 0 or ballrect.bottom > HEIGHT:
+    dimmension[i] = ballrect[i].move(speed)
+    if dimmension[i].left < 0 or dimmension[i].right > WIDTH:
+        speed[0] = -speed[0]
+        if speed[1] < 0:
+            speed[1] = random.randint(1,20)
             speed[1] = -speed[1]
-            if speed[0] < 0:
-                speed[0] = random.randint(1,20)
-                speed[0] = -speed[0]
-            else:
-                speed[0] = random.randint(1,20)
-    
+        else:
+            speed[1] = random.randint(1,20)
+
+    if dimmension[i].top < 0 or dimmension[i].bottom > HEIGHT:
+        speed[1] = -speed[1]
+        if speed[0] < 0:
+            speed[0] = random.randint(1,20)
+            speed[0] = -speed[0]
+        else:
+            speed[0] = random.randint(1,20)
+
     screen.fill(black)
-    screen.blit(ball, ballrect)
+    screen.blit(file_names[i], dimmension[i])
     pygame.display.flip()
+    print i
+    print n
+    print ballrect[i]
+    print dimmension[i]
+    print file_names
+    i += 1
+    n += 1
+    
     

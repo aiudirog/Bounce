829f2d63ca4f21df6bafed30808c81c4f66f265e
pydev <pydev@MACLAB07.local>
2013-09-24 12:18:48 -0400

    Commit

--- bounce.py
+++ bounce.py
@@ -5,6 +5,9 @@ pygame.init()
 ballrect = []
 dimmension = []
 image_list = []
+file_names = []
+x_speed = []
+y_speed = []
 
 screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN) 
 WIDTH, HEIGHT = screen.get_size() 
@@ -15,18 +18,20 @@ background = 255, 255, 255
 
 
 #Create a list of all images in dir.balls.
-os.chdir("/balls")
-for files in os.listdir("."):
+for files in os.listdir("Balls"):
     if files.endswith(".png"):
         image_list.append(files)
 
 #Load all images.
 count = 0
 for image in image_list:
-   file_names.append(pygame.image.load(os.path.join("Balls" , image_list[count])))
-   x_speed.append(2)
-   y_speed.append(2)
-   count += 1
+    dir = os.path.dirname(__file__)
+    print "This is the dir:", dir
+    print image_list
+    file_names.append(pygame.image.load(os.path.join(dir, "Balls" , image_list[count])))
+    x_speed.append(21)
+    y_speed.append(21)
+    count += 1
 
 x = 2
 
@@ -58,8 +63,24 @@ while True:
         
 #Change direction and speed when encountering a wall.
 #This works. I know that.--------------------------
-        if dimmension[i].left < 0 or dimmension[i].right > WIDTH:
+        if dimmension[i].left < 0 and dimmension[i].top < 0:
             x_speed[i] = -x_speed[i]
+            y_speed[i] = -y_speed[i]
+        if dimmension[i].left < 0 and dimmension[i].bottom > HEIGHT:
+            x_speed[i] = -x_speed[i]
+            y_speed[i] = -y_speed[i]
+        if dimmension[i].right > WIDTH  and dimmension[i].top < 0:
+            x_speed[i] = -x_speed[i]
+            y_speed[i] = -y_speed[i]
+        if dimmension[i].right > WIDTH and dimmension[i].bottom > HEIGHT:
+            x_speed[i] = -x_speed[i]
+            y_speed[i] = -y_speed[i]
+        if dimmension[i].left < 0 or dimmension[i].right > WIDTH:
+            if x_speed[i] < 0:
+                x_speed[i] = random.randint(1,20)
+            else:
+                x_speed[i] = random.randint(1,20)
+                x_speed[i] = -x_speed[i]
             if y_speed[i] < 0:
                 y_speed[i] = random.randint(1,20)
                 y_speed[i] = -y_speed[i]
@@ -67,7 +88,11 @@ while True:
                 y_speed[i] = random.randint(1,20)
 
         if dimmension[i].top < 0 or dimmension[i].bottom > HEIGHT:
-            y_speed[i] = -y_speed[i]
+            if y_speed[i] < 0:
+                y_speed[i] = random.randint(1,20)
+            else:
+                y_speed[i] = random.randint(1,20)
+                y_speed[i] = -y_speed[i]
             if x_speed[i] < 0:
                 x_speed[i] = random.randint(1,20)
                 x_speed[i] = -x_speed[i]
@@ -90,13 +115,13 @@ while True:
 #Debugging output:
 #-------------------
     #print i
-    print n
-    print x_speed
-    print y_speed
+    #print n
+    #print x_speed
+    #print y_speed
     #print ballrect[i]
     #print dimmension[i]
-    print file_names
-    n += 1
+    #print file_names
+    #n += 1
 #--------------------
     i = 0
 
